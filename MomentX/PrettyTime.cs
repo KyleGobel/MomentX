84f39e1158aba5eea419142fd06f066b65f544e5
Kyle Gobel <kgobel@gmail.com>
2013-12-29 09:16:55 -0500

    added pretty time

---
+++ MomentX/PrettyTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MomentX
+{
+    public static class PrettyTimeExtension
+    {
+        public static string PrettyTime(this TimeSpan This)
+        {
+            if (This < TimeSpan.FromSeconds(10))
+                return Math.Round(This.TotalMilliseconds) + "ms";
+            if (This < TimeSpan.FromMinutes(1))
+                return "about " + Math.Round(This.TotalSeconds) + " seconds";
+            if (This < TimeSpan.FromHours(1))
+                return "about " + Math.Round(This.TotalMinutes) + "m " + This.Seconds + " s";
+
+            return This.Days + "d:" + This.Hours + "h:" + This.Minutes + "m:" + This.Seconds + "s";
+
+        }
+    }
+}
